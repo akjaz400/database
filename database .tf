47968f8e68b3877bcb23120204b36d6957bc8ce6
akjaz400 <akjaz400@outlook.com>
2024-05-03 15:52:28 +0000

    3

--- database .tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_mssql_database" "mssql" {
-  name                = "techonedb-mssql"
-  resource_group_name = azurerm_resource_group.dbrgsql.name
-  location            = azurerm_resource_group.dbrgsql.location
-  server_name         = azurerm_mssql_server.techone.name
-
-}
